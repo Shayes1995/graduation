bf0a633b1812ae3197328874050ee22731070609
Shayan S <shayans-s@outlook.com>
2025-01-14 15:02:25 +0100

    protected route

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Admin from './pages/Admin';
 import Register from './pages/Register';
 import AdminAddPosts from './pages/AdminAddPosts';
 import MinSida from './pages/MinSida';
+import ProtectedRoute from './ProtectedRoute/ProtectedRoute';
 
 const App = () => {
   const router = createBrowserRouter([
@@ -30,9 +31,14 @@ const App = () => {
           element: <Admin />
         },
         {
-          path: 'admin/add-posts', // Nu är 'admin' en relativ path
-          element: <AdminAddPosts />
+          path: 'admin/add-posts',
+          element: (
+            <ProtectedRoute requiredRole="admin">
+              <AdminAddPosts />
+            </ProtectedRoute>
+          ),
         },
+        
         {
           path: 'register', // Nu är 'register' en relativ path
           element: <Register />

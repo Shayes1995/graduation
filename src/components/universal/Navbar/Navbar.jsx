c9a0c70ea1627f9af50df1261c6643ef57d691e7
kdb11 <112870790+kdb11@users.noreply.github.com>
2025-01-14 16:20:43 +0100

    hover

--- src/components/universal/Navbar/Navbar.jsx
+++ src/components/universal/Navbar/Navbar.jsx
@@ -4,17 +4,14 @@ import contactBlack from './contact-black.svg';
 import { Link, useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
-
 const Navbar = () => {
   const user = Cookies.get('user');
   const navigate = useNavigate();
-
 
   const handleLogout = () => {
     Cookies.remove('user');
     navigate('/login');
   };
-
 
   return (
     <header className="pt-3 mb-4">
@@ -25,17 +22,15 @@ const Navbar = () => {
             </Link>
           </div>
 
-
-          <ul className="nav col-12 col-md-auto mb-3 justify-content-center mb-md-0">
-            <li><Link to="#" className="nav-link px-2 link-secondary">Lediga jobb</Link></li>
-            <li><Link to="#" className="nav-link px-2">Academy</Link></li>
-            <li><Link to="#" className="nav-link px-2">För jobbsökande</Link></li>
-            <li><Link to="#" className="nav-link px-2">För företag</Link></li>
-            <li><Link to="#" className="nav-link px-2">Om oss</Link></li>
-            <li><Link to="#" className="nav-link px-2">Kontakt</Link></li>
+          <ul className="nav col-12 fs-6 col-md-auto mb-3 justify-content-center mb-md-0">
+            <li><Link to="#" className="nav-link px-2 link-dark text-hover">Lediga jobb</Link></li>
+            <li><Link to="#" className="nav-link px-2 link-dark">Academy</Link></li>
+            <li><Link to="#" className="nav-link px-2 link-dark">För jobbsökande</Link></li>
+            <li><Link to="#" className="nav-link px-2 link-dark">För företag</Link></li>
+            <li><Link to="#" className="nav-link px-2 link-dark">Om oss</Link></li>
+            <li><Link to="#" className="nav-link px-2 link-dark">Kontakt</Link></li>
             {user && <li><Link to="/min-sida" className="nav-link px-3">Min sida</Link></li>}
           </ul>
-
 
           <div className="col-md-3 text-end">
             {user ? (
@@ -51,6 +46,4 @@ const Navbar = () => {
   )
 }
 
-
 export default Navbar;
-

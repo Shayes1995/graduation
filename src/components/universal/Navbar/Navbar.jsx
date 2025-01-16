b43d232f7e241bb4d74ccdf9c8762bbcc5230621
Evelina <evelina.enqvist@brightstraining.com>
2025-01-16 13:45:21 +0100

    Active link navbar

--- src/components/universal/Navbar/Navbar.jsx
+++ src/components/universal/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import LogoMainGreen from './logo_main_green.svg';
 import contactBlack from './contact-black.svg';
-import { Link, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
 const Navbar = () => {
@@ -17,38 +17,35 @@ const Navbar = () => {
 
   return (
     <header className="pt-3 mb-4 navBar">
-        <div className="d-flex align-items-center mx-3 justify-content-center justify-content-md-between">
-          <div className="col-md-3 mb-2 mb-md-0">
-            <Link to="/" className="d-inline-flex link-body-emphasis text-decoration-none">
-              <img src={LogoMainGreen} alt="Main Logo" width="160" height="64" />
-            </Link>
-          </div>
-
-          <ul className="nav col-12 fs-6 col-md-auto mb-3 justify-content-center mb-md-0">
-            <li><Link to="#" className="nav-link px-2 link-dark text-hover">Lediga jobb</Link></li>
-            <li><Link to="#" className="nav-link px-2 link-dark">Academy</Link></li>
-            <li><Link to="#" className="nav-link px-2 link-dark">För jobbsökande</Link></li>
-            <li><Link to="#" className="nav-link px-2 link-dark">För företag</Link></li>
-            <li><Link to="#" className="nav-link px-2 link-dark">Om oss</Link></li>
-            {user && <li><Link to="/my-page" className="nav-link px-2 link-dark">Min sida</Link></li>}
-            {/* {admin && <li><Link to="/admin/add-posts" className="nav-link px-2 link-dark">Add job</Link></li>}
-            {admin && <li><Link to="/admin/search" className="nav-link px-2 link-dark">Search Users</Link></li>} */}
-            <li><Link to="/candidate" className="nav-link px-2 link-dark">Kontakt</Link></li>
-            {user && <li><Link to="/min-sida" className="nav-link px-2 link-dark">Min sida</Link></li>}
-          </ul>
-
-          <div className="col-md-3 text-end">
-            {user || admin ? (
-              <button onClick={handleLogout} className="btn btn-secondary">Logout</button>
-            ) : (
-              <Link to="/login" className="d-inline-flex link-body-emphasis text-decoration-none">
-                <img src={contactBlack} alt="Contact Black" width="40" height="32" />
-              </Link>
-            )}
-          </div>
+      <div className="d-flex align-items-center mx-3 justify-content-center justify-content-md-between">
+        <div className="col-md-3 mb-2 mb-md-0">
+          <NavLink to="/" className="d-inline-flex link-body-emphasis text-decoration-none">
+            <img src={LogoMainGreen} alt="Main Logo" width="160" height="64" />
+          </NavLink>
         </div>
+
+        <ul className="nav col-12 fs-6 col-md-auto mb-3 justify-content-center mb-md-0">
+          <li><NavLink to="/jobs" className="nav-link px-2 link-dark" activeClassName="active">Lediga jobb</NavLink></li>
+          <li><NavLink to="/academy" className="nav-link px-2 link-dark" activeClassName="active">Academy</NavLink></li>
+          <li><NavLink to="/candidates" className="nav-link px-2 link-dark" activeClassName="active">För jobbsökande</NavLink></li>
+          <li><NavLink to="/companies" className="nav-link px-2 link-dark" activeClassName="active">För företag</NavLink></li>
+          <li><NavLink to="/about" className="nav-link px-2 link-dark" activeClassName="active">Om oss</NavLink></li>
+          {user && <li><NavLink to="/my-page" className="nav-link px-2 link-dark" activeClassName="active">Min sida</NavLink></li>}
+          <li><NavLink to="/contact" className="nav-link px-2 link-dark" activeClassName="active">Kontakt</NavLink></li>
+        </ul>
+
+        <div className="col-md-3 text-end">
+          {user || admin ? (
+            <button onClick={handleLogout} className="btn btn-secondary">Logout</button>
+          ) : (
+            <NavLink to="/login" className="d-inline-flex link-body-emphasis text-decoration-none">
+              <img src={contactBlack} alt="Contact Black" width="40" height="32" />
+            </NavLink>
+          )}
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
 export default Navbar;

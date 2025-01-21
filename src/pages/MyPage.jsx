bca289feb4140a2fbd63039612baaabd92dffbcc
Evelina <evelina.enqvist@brightstraining.com>
2025-01-21 17:22:26 +0100

    Update

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -92,7 +92,6 @@ const MyPage = () => {
     return (
         <div className="my-page-container">
             <h1>Välkommen {firstName || 'Användare'}</h1>
-            
             <p>Håll din profil uppdaterad för att ge andra en bättre förståelse för vem du är. Lägg till dina färdigheter och dela din CV-länk för att skapa ett starkt intryck.</p>
 
             <div className="profile-card">
